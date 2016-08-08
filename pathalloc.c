b1a73c7d00db8c6810b800cea0eb14f520b7feb6
micro-hxq <hxqmicro@gmail.com>
2016-08-08 18:22:54 +0800

    update

--- pathalloc.c
+++ pathalloc.c
@@ -11,11 +11,10 @@
     static long posix_version = 0;
     static long xsi_version = 0;
 
-    /* If PATH_MAX is indeterminate, no guarantee this is adequate */
     #define PATH_MAX_GUESS  1024
 
     char *
-    path_alloc(size_t *sizep) /* also return allocated size, if nonnull */
+    path_alloc(size_t *sizep) 
     {
         char    *ptr;
         size_t  size;
@@ -26,22 +25,18 @@
         if (xsi_version == 0)
             xsi_version = sysconf(_SC_XOPEN_VERSION);
 
-        if (pathmax == 0) {     /* first time through */
+        if (pathmax == 0) {     
             errno = 0;
             if ((pathmax = pathconf("/", _PC_PATH_MAX)) < 0) {
                 if (errno == 0)
-                    pathmax = PATH_MAX_GUESS;   /* it's indeterminate */
+                    pathmax = PATH_MAX_GUESS;  
                 else
                     err_sys("pathconf error for _PC_PATH_MAX");
             } else {
-                pathmax++;      /* add one since it's relative to root */
+                pathmax++; 
             }
         }
 
-        /*
-         * Before POSIX.1-2001, we aren't guaranteed that PATH_MAX includes
-         * the terminating null byte.  Same goes for XPG3.
-         */
         if ((posix_version < 200112L) && (xsi_version < 4))
             size = pathmax + 1;
         else
